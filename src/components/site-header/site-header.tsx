1c6118f54f9e889d490d63c8c3c105553b0a2854
Cam Wiegert <cam@camwiegert.com>
2018-04-17 11:34:28 -0500

    Move icons to src/icons.tsx

--- src/components/site-header/site-header.tsx
+++ src/components/site-header/site-header.tsx
@@ -1,34 +1,6 @@
 import { Component, Prop } from '@stencil/core';
 import { SECTION_FRAMEWORK, SECTION_CLI } from '../../constants';
-
-const CloseIcon = () => (
-  <svg viewBox="0 0 54 54">
-    <rect transform="rotate(45 27 27)" y="22" width="54" height="10" rx="2"/>
-    <rect transform="rotate(-45 27 27)" y="22" width="54" height="10" rx="2"/>
-  </svg>
-);
-
-const MenuIcon = () => (
-  <svg viewBox="0 0 54 54">
-    <rect y="0" width="54" height="10" rx="2"/>
-    <rect y="22" width="54" height="10" rx="2"/>
-    <rect y="44" width="54" height="10" rx="2"/>
-  </svg>
-);
-
-const NewTabIcon = () => (
-  <svg class="new-tab" viewBox="0 0 43 42">
-    <rect class="new-tab__box" y="8" width="34" height="34" rx="6"/>
-    <path class="new-tab__arrow" d="M37.078 3.268H23.617V.243h18.626v18.625h-3.026V5.407L16.13 28.494l-2.14-2.139z"/>
-  </svg>
-);
-
-const IonicLogo = () => (
-  <svg viewBox="0 0 144 144">
-    <title>Ionic</title>
-    <path d="M72 39c18.183 0 33 14.817 33 33s-14.79 33-33 33-33-14.817-33-33c0-18.21 14.817-33 33-33zm46.5 2c-8.008 0-14.5-6.492-14.5-14.5S110.492 12 118.5 12 133 18.492 133 26.5 126.508 41 118.5 41zm19.031 1.16C141.834 51.58 144 61.621 144 72c0 39.713-32.287 72-72 72-39.712 0-72-32.287-72-72C0 32.288 32.288 0 72 0c11.616 0 22.725 2.7 32.99 7.987l1.35.704-1.18.956c-2.897 2.334-5.204 5.344-6.638 8.719l-.394.928-.928-.45c-7.903-3.769-16.397-5.682-25.2-5.682-32.428 0-58.837 26.41-58.837 58.838s26.353 58.81 58.809 58.81c32.456 0 58.837-26.41 58.837-58.838a58.49 58.49 0 0 0-4.5-22.556l-.393-.928.956-.366a22.562 22.562 0 0 0 9.028-6.216l1.012-1.125.62 1.378-.001.001z" fill-rule="nonzero"/>
-  </svg>
-);
+import { Close, Ionic, Menu, NewTab } from '../../icons';
 
 const frameworkDropdownItems = [
   {
@@ -124,7 +96,7 @@ export class SiteHeader {
             <a href={item.url}>
               <strong>{item.title}</strong>
               <span>{item.subtitle}</span>
-              { item.category ? null : <NewTabIcon/> }
+              { item.category ? null : <NewTab/> }
             </a>
           </li>
         )}
@@ -161,9 +133,9 @@ export class SiteHeader {
         <button
           onClick={this.onToggleClick}
           class={{ 'site-nav-toggle': true, 'is-open': this.isMenuOpen }}>
-            { this.isMenuOpen ? <CloseIcon/> : <MenuIcon/> }
+            { this.isMenuOpen ? <Close/> : <Menu/> }
         </button>
-        <a href="/docs" id="site-logo"><IonicLogo/></a>
+        <a href="/docs" id="site-logo"><Ionic/></a>
         <ctrl-dropdown
           class="framework-dropdown"
           autoClose
@@ -175,7 +147,7 @@ export class SiteHeader {
           autoClose
           items={ecosystemDropdownItems}
           renderer={this.renderEcosystemDropdown}/>
-        <a href={this.getGithubLink()} class="github" target="_blank">GitHub <NewTabIcon/></a>
+        <a href={this.getGithubLink()} class="github" target="_blank">GitHub <NewTab/></a>
       </nav>
     );
   }
